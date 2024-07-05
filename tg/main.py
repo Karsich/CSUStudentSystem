5bba6b13f683f07f2b31814b590cf283c2d3ac0d
KarSich <hryhcev@bk.ru>
2024-07-05 11:08:11 +0300

    сервис

--- tg/main.py
+++ tg/main.py
@@ -1,30 +1,19 @@
 import asyncio
 import logging
-import requests
-from fastapi import FastAPI
-from fastapi.responses import Response
-
-import faq_helper
-import logging
-from aiogram.client.default import DefaultBotProperties
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, Update
-from aiogram.filters import CommandStart
-from aiogram.enums import ParseMode
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
-import uvicorn
-from faq_helper import search_faq
-from aiogram import Bot, Dispatcher, types, Router, F
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.filters import Command
+from contextlib import asynccontextmanager
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup, default_state
+
 from config import BOT_TOKEN, NGROK_TUNNEL_URL
-from contextlib import asynccontextmanager
+from service import APIService
 
 API_TOKEN = BOT_TOKEN
 
@@ -32,20 +21,16 @@ API_TOKEN = BOT_TOKEN
 WEBHOOK_HOST = NGROK_TUNNEL_URL
 WEBHOOK_PATH = '/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBHOOKO_URL = f"{WEBHOOK_HOST}/bot/{API_TOKEN}"
 
 # Настройки веб-сервера
 WEBAPP_HOST = 'localhost'
 WEBAPP_PORT = 5000
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=API_TOKEN,
-          default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Credentials for login
 login_url = 'http://95.174.92.220:8000/auth/login'
-department_url = 'http://95.174.92.220:8000/departments'
 groups_url = 'http://95.174.92.220:8000/groups'
 login_data = {
     "username": "telegram",
@@ -55,116 +40,62 @@ login_data = {
     "password": "awJwnbbT"
 }
 
-# URL endpoint for FAQ
-faq_url = 'http://95.174.92.220:8000/faqs'
-headers = {'Content-Type': 'application/json'}
-jwt_token = None
+api_service = APIService(login_url, groups_url, login_data)
 
-# Function to update JWT token
-def update_token():
-    global jwt_token
-    response = requests.post(login_url, json=login_data, headers=headers)
-    if response.status_code == 200:
-        jwt_token = response.json().get('access_token')
-        headers['Authorization'] = f'Bearer {jwt_token}'
-        logging.info('JWT token updated successfully.')
-    else:
-        logging.error(f'Failed to update JWT token: {response.status_code} - {response.text}')
-
-# Update token initially and then every 5 minutes
-
-# States
 class Form(StatesGroup):
     group = State()
     name = State()
-    department = State()
     student_id = State()
 
-#with open('groups.txt', 'r', encoding='utf-8') as f:
-#    groups = sorted([line.strip() for line in f.readlines()])
-
-# Keyboards
 main_b = [
-    [InlineKeyboardButton(text="Подать заявку в группу",callback_data='fill_form')],
-    [InlineKeyboardButton(text="FAQ",callback_data='FAQ')]
-]
-verification_b = [
-    [InlineKeyboardButton(text="Верификация", callback_data='fill_form')]
-]
-faq_b = [
-    #[InlineKeyboardButton(text="Просмотр всех вопросов", callback_data='view_all')],
-    [InlineKeyboardButton(text="Запрос", callback_data='request')]
-]
-notifications_b = [
-    [InlineKeyboardButton(text="Показать непрочтенные", callback_data='show_unread')],
-    [InlineKeyboardButton(text="Показать все", callback_data='show_all')]
+    [InlineKeyboardButton(text="Подать заявку в группу", callback_data='fill_form')],
+    [InlineKeyboardButton(text="FAQ", callback_data='FAQ')]
 ]
 
 main_kb = InlineKeyboardMarkup(inline_keyboard=main_b)
-verification_kb = InlineKeyboardMarkup(inline_keyboard=verification_b)
-faq_kb = InlineKeyboardMarkup(inline_keyboard=faq_b)
-notifications_kb = InlineKeyboardMarkup(inline_keyboard=notifications_b)
-temp_departments = []
-temp_groups = []
 
-# Start command handler
-@dp.message(Command("start"))
+@dp.message(CommandStart())
 async def send_welcome(message: types.Message):
-    response = requests.get(f'http://95.174.92.220:8000/students/{message.from_user.id}', headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if data:
-            await message.answer("Привет! Это информационная система для института. Пожалуйста, выберите опцию.",
-                                 reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Уведомления")]], resize_keyboard=True))
-        else:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'http://95.174.92.220:8000/students/{message.from_user.id}', headers=api_service.headers)
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                await message.answer("Привет! Это информационная система для института. Пожалуйста, выберите опцию.",
+                                     reply_markup=types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="Уведомления")]], resize_keyboard=True))
+            else:
+                await message.answer("Привет! Это информационная система для института. Пожалуйста, выберите опцию.",
+                                     reply_markup=main_kb)
+        elif response.status_code == 403:
             await message.answer("Привет! Это информационная система для института. Пожалуйста, выберите опцию.",
                                  reply_markup=main_kb)
-    elif response.status_code == 403:
-        await message.answer("Привет! Это информационная система для института. Пожалуйста, выберите опцию.",
-                             reply_markup=main_kb)
-    else:
-        await message.answer("Ошибка при запросе: " + str(response.status_code) + " - " + response.text)
+        else:
+            await message.answer("Ошибка при запросе: " + str(response.status_code) + " - " + response.text)
 
-# Verification process
-#@dp.message(F.text == "Верификация")
-#async def process_verification(message: types.Message):
-#    await message.answer("Для доступа к функциям, пожалуйста, пройдите верификацию.", reply_markup=verification_kb)
-
-# в будущем изменить
-@dp.callback_query(F.data == "fill_form")
+@dp.callback_query(lambda c: c.data == "fill_form")
 async def fill_form(callback_query: types.CallbackQuery, state: FSMContext):
     await state.set_state(Form.group)
-    update_token()
-    response = requests.get(groups_url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        groups = [item['short_name'] for item in data]
-        groups_kb = await get_groups_kb(groups)
-        await bot.send_message(callback_query.from_user.id, "Выберите группу:", reply_markup=groups_kb)
-    else:
-        await bot.send_message(callback_query.from_user.id, "Ошибка при запросе: " + str(response.status_code) + " - " + response.text)
+    await api_service.update_token()
+    groups = await api_service.get_groups()
+    groups_kb = await get_groups_kb([item['short_name'] for item in groups])
+    await bot.send_message(callback_query.from_user.id, "Выберите группу:", reply_markup=groups_kb)
 
-@dp.callback_query(F.data.startswith("choose_group"))
+@dp.callback_query(lambda c: c.data.startswith("choose_group"))
 async def process_group(callback_query: types.CallbackQuery, state: FSMContext):
     group = callback_query.data.replace("choose_group", "")
-    response = requests.get(f"{groups_url}/{group}", headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        department = data['department']
-        specialty = data['specialty']
-        members_count = data['user_count']
-        await bot.send_message(callback_query.from_user.id, f"Группа: {group}\nДепартамент: {department}\nСпециальность: {specialty}\nКоличество участников: {members_count}")
-        join_kb = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Да", callback_data=f"join_group{group}")],
-            [InlineKeyboardButton(text="Нет", callback_data="fill_form")]
-        ])
-        await bot.send_message(callback_query.from_user.id, "Хотите присоединиться к этой группе?", reply_markup=join_kb)
-    else:
-        await bot.send_message(callback_query.from_user.id, "Ошибка при запросе: " + str(response.status_code) + " - " + response.text)
+    data = await api_service.get_group_details(group)
+    department = data['department']
+    specialty = data['specialty']
+    members_count = data['user_count']
+    await bot.send_message(callback_query.from_user.id, f"Группа: {group}\nДепартамент: {department}\nСпециальность: {specialty}\nКоличество участников: {members_count}")
+    join_kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Да", callback_data=f"join_group{group}")],
+        [InlineKeyboardButton(text="Нет", callback_data="fill_form")]
+    ])
+    await bot.send_message(callback_query.from_user.id, "Хотите присоединиться к этой группе?", reply_markup=join_kb)
 
-@dp.callback_query(F.data.startswith("join_group"))
+@dp.callback_query(lambda c: c.data.startswith("join_group"))
 async def join_group(callback_query: types.CallbackQuery, state: FSMContext):
-    # Send the data to the admin panel for verification
     await state.update_data(group=callback_query.data.replace("join_group", ""))
     await state.set_state(Form.name)
     await bot.send_message(callback_query.from_user.id, "Пожалуйста, введите ваше ФИО:")
@@ -175,7 +106,7 @@ async def process_name(message: types.Message, state: FSMContext):
     await state.set_state(Form.student_id)
     await message.answer("Пожалуйста, отправьте фотографию вашего студенческого билета:")
 
-@dp.message(Form.student_id, F.photo)
+@dp.message(Form.student_id, lambda message: message.photo)
 async def process_student_id(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
     group = user_data['group']
@@ -183,21 +114,16 @@ async def process_student_id(message: types.Message, state: FSMContext):
     student_id_photo = message.photo[-1].file_id
     tg_chat = message.chat.id
 
-    # Send the data to the admin panel for verification
     ticket_data = {
         'type_ticket': 'verification',
         'tgchat_id': tg_chat,
         'wish_group': group,
         'fullname': name,
-        #    'photo': student_id_photo
     }
-    update_token()
-    response = requests.post('http://95.174.92.220:8000/tickets', json=ticket_data)
-
+    response = await api_service.submit_ticket(ticket_data)
     if response.status_code == 200:
-        response_trust = requests.get(f'http://95.174.92.220:8000/tickets/{tg_chat}/active')
-        dataa = response_trust.json()
-        if response_trust.status_code == 200 & dataa:
+        dataa = await api_service.check_active_ticket(tg_chat)
+        if dataa:
             await message.answer("Заявка была отправлена ранее")
         else:
             await message.answer(f"Заявка отправлена на рассмотрение.\nГруппа: {group}\nФИО: {name}\n Фото: {student_id_photo}")
@@ -206,117 +132,30 @@ async def process_student_id(message: types.Message, state: FSMContext):
 
     await state.clear()
 
-# FAQ process
-@dp.callback_query(F.data == "FAQ")
+@dp.callback_query(lambda c: c.data == "FAQ")
 async def process_faq(callback_query: types.CallbackQuery):
-    await bot.send_message(callback_query.from_user.id, "Выберите опцию:", reply_markup=faq_kb)
+    await bot.send_message(callback_query.from_user.id, "Выберите опцию:", reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Ввести запрос", callback_data="request")]
+    ]))
 
-#@dp.callback_query(F.data == "view_all")
-#async def view_all(callback_query: types.CallbackQuery):
-    # Выполняем GET запрос к API с заголовком авторизации
-    #    response = requests.get(faq_url, headers=headers)
-    # Проверяем успешность запроса
-    #    if response.status_code == 200:
-    #        data = response.json()
-    #        faqs = "\n\n".join([f"Вопрос: {item['question']}\nОтвет: {item['answer']}" for item in data])
-    #       await bot.send_message(callback_query.from_user.id, f"Список всех вопросов и ответов:\n{faqs}")
-    #   else:
-#        await bot.send_message(callback_query.from_user.id, f"Ошибка при запросе: {response.status_code} - {response.text}")
-
-@dp.callback_query(F.data == "request")
+@dp.callback_query(lambda c: c.data == "request")
 async def request_faq(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, "Введите ваш запрос:")
 
 @dp.message(default_state)
 async def search_faq(message: types.Message):
     query = message.text
-    faq_data = {
-        'message': query
-    }
-    # Выполняем GET запрос к API с заголовком авторизации
-    update_token()
-    response = requests.post('http://95.174.92.220:8000/faqs/search', json=faq_data)
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        data = response.json()
-        for result in data:
-             await message.answer(f"Вопрос: {result['question']}\nОтвет: {result['answer']}")
-        #results = faq_helper.search_faq(query, data)[:5]  # Call the search_faq function with the pack
-        #if results:
-        #    await message.answer(f"Результаты поиска для запроса '{query}':")
-        #    for result in results:
-        #        await message.answer(f"Вопрос: {result['question']}\nОтвет: {result['answer']}")
-        #else:
-        #    await message.answer(f"Не найдено результатов для запроса '{query}'.")
-    else:
-        await message.answer(f"Ошибка при запросе: {response.status_code} - {response.text}")
-
-async def get_department_kb(departments):
-    department_kb = InlineKeyboardMarkup(
-        inline_keyboard=[
-                            [InlineKeyboardButton(text=department, callback_data="choose_department" + str(i)) for
-                             department in departments[i:i + 1]]
-                            for i in range(0, len(departments), 1)
-                        ]
-    )
-    return department_kb
+    results = await api_service.search_faq(query)
+    for result in results:
+        await message.answer(f"Вопрос: {result['question']}\nОтвет: {result['answer']}")
 
 async def get_groups_kb(groups):
     groups_kb = InlineKeyboardMarkup(
         inline_keyboard=[
-                            [InlineKeyboardButton(text=group, callback_data="choose_group" + group) for group in
-                             groups[i:i + 1]]
-                            for i in range(0, len(groups), 1)
-                        ]
+            [InlineKeyboardButton(text=group, callback_data="choose_group" + group)] for group in groups
+        ]
     )
     return groups_kb
 
-# Notifications process
-@dp.message(F.text == "Уведомления")
-async def process_notifications(message: types.Message):
-    await message.answer("Выберите опцию:", reply_markup=notifications_kb)
-
-@dp.callback_query(F.data == "show_unread")
-async def show_unread(callback_query: types.CallbackQuery):
-    # Fetch and display unread notifications
-    await bot.send_message(callback_query.from_user.id, "Непрочитанные уведомления...")
-
-@dp.callback_query(F.data == "show_all")
-async def show_all(callback_query: types.CallbackQuery):
-    # Fetch and display all notifications
-    await bot.send_message(callback_query.from_user.id, "Все уведомления...")
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await bot.set_webhook(url=WEBHOOK_URL,
-                          allowed_updates=dp.resolve_used_update_types(),
-                          drop_pending_updates=True)
-    yield
-    await bot.delete_webhook()
-
-
-app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
-@dp.message(CommandStart())
-async def start(message: Message) -> None:
-    await message.answer('Привет!')
-
-
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-async def main():
-    await bot.delete_webhook()
-    await dp.start_polling(bot)
-@app.post("/webhook")
-async def webhook(request: Request) -> None:
-    update = Update.model_validate(await request.json(), context={"bot": bot})
-    await dp.feed_update(bot, update)
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    dp.run_polling(bot)
---
+++ tg/service.py
@@ -0,0 +1,55 @@
+import httpx
+import logging
+
+class APIService:
+    def __init__(self, login_url, groups_url, login_data):
+        self.client = httpx.AsyncClient()
+        self.login_url = login_url
+        self.groups_url = groups_url
+        self.login_data = login_data
+        self.token = None
+        self.headers = {'Content-Type': 'application/json'}
+
+    async def update_token(self):
+        response = await self.client.post(self.login_url, json=self.login_data, headers=self.headers)
+        if response.status_code == 200:
+            self.token = response.json().get('access_token')
+            self.headers['Authorization'] = f'Bearer {self.token}'
+            logging.info('JWT token updated successfully.')
+        else:
+            logging.error(f'Failed to update JWT token: {response.status_code} - {response.text}')
+
+    async def get_groups(self):
+        if not self.token:
+            await self.update_token()
+        response = await self.client.get(self.groups_url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    async def get_group_details(self, group_id):
+        if not self.token:
+            await self.update_token()
+        response = await self.client.get(f"{self.groups_url}/{group_id}", headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    async def submit_ticket(self, ticket_data):
+        if not self.token:
+            await self.update_token()
+        response = await self.client.post('http://95.174.92.220:8000/tickets', json=ticket_data, headers=self.headers)
+        return response
+
+    async def check_active_ticket(self, tg_chat):
+        if not self.token:
+            await self.update_token()
+        response = await self.client.get(f'http://95.174.92.220:8000/tickets/{tg_chat}/active', headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    async def search_faq(self, query):
+        if not self.token:
+            await self.update_token()
+        faq_data = {'message': query}
+        response = await self.client.post('http://95.174.92.220:8000/faqs/search', json=faq_data, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
